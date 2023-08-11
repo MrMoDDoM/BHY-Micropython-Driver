b8fcfc72930efbc4944f300ebf389151ce663ac6
MrMoDDoM <danielebarattieri@gmail.com>
2023-08-11 12:22:15 +0200

    Added license header to file

--- bhy.py
+++ bhy.py
@@ -1,3 +1,17 @@
+# BHY library for MicroPython
+# Copyright (C) 2021 MrMoDDoM
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License v3.0
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 from time import sleep_ms
 from machine import I2C, Pin
 import binascii
